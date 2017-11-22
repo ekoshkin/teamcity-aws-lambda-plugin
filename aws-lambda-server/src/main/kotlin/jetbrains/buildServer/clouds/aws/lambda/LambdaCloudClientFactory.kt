187327b7842fad777f9dc9f772e457f924330d64
ekoshkin <koshkinev@gmail.com>
2017-11-23 00:45:01 +0300

    entry point works

--- aws-lambda-server/src/main/kotlin/jetbrains/buildServer/clouds/aws/lambda/LambdaCloudClientFactory.kt
+++ aws-lambda-server/src/main/kotlin/jetbrains/buildServer/clouds/aws/lambda/LambdaCloudClientFactory.kt
@@ -1,16 +1,26 @@
 package jetbrains.buildServer.clouds.aws.lambda
 
-import jetbrains.buildServer.clouds.CloudClientEx
-import jetbrains.buildServer.clouds.CloudClientFactory
-import jetbrains.buildServer.clouds.CloudClientParameters
-import jetbrains.buildServer.clouds.CloudState
+import jetbrains.buildServer.clouds.*
 import jetbrains.buildServer.serverSide.AgentDescription
+import jetbrains.buildServer.serverSide.InvalidProperty
 import jetbrains.buildServer.serverSide.PropertiesProcessor
+import jetbrains.buildServer.serverSide.ServerSettings
+import jetbrains.buildServer.util.amazon.AWSCommonParams
+import jetbrains.buildServer.web.openapi.PluginDescriptor
+import java.util.*
 
 /**
  * Created by Evgeniy Koshkin (evgeniy.koshkin@jetbrains.com) on 23.11.17.
  */
-class LambdaCloudClientFactory : CloudClientFactory {
+class LambdaCloudClientFactory(cloudRegister: CloudRegistrar,
+                               pluginDescriptor: PluginDescriptor,
+                               private val serverSettings: ServerSettings) : CloudClientFactory {
+    private val editUrl = pluginDescriptor.getPluginResourcesPath("editLambda.html")
+
+    init {
+        cloudRegister.registerCloudFactory(this)
+    }
+
     override fun getCloudCode(): String {
         return "awslambda"
     }
@@ -20,11 +30,23 @@ class LambdaCloudClientFactory : CloudClientFactory {
     }
 
     override fun getEditProfileUrl(): String? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return editUrl
     }
 
     override fun getInitialParameterValues(): MutableMap<String, String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val result = HashMap<String, String>()
+        result.putAll(AWSCommonParams.getDefaults(serverSettings.serverUUID))
+        return result
+    }
+
+    override fun getPropertiesProcessor(): PropertiesProcessor {
+        return PropertiesProcessor { properties ->
+            val invalids = ArrayList<InvalidProperty>()
+            for (e in AWSCommonParams.validate(properties!!, false)) {
+                invalids.add(InvalidProperty(e.key, e.value))
+            }
+            invalids
+        }
     }
 
     override fun canBeAgentOfType(description: AgentDescription): Boolean {
@@ -32,10 +54,6 @@ class LambdaCloudClientFactory : CloudClientFactory {
     }
 
     override fun createNewClient(state: CloudState, params: CloudClientParameters): CloudClientEx {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getPropertiesProcessor(): PropertiesProcessor {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
